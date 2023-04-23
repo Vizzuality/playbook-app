99e1dbcd719250eee1e1ba33b60ebafc536ddb71
CV-Gate <miguel.mendoza@vizzuality.com>
2023-04-23 21:07:59 +0200

    removes private from file names

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 import re
 
-def humanize(s):
-    return re.sub(r'[-_]', ' ', s).title()
+def humanize(item):
+    item = re.sub(r'^private\s*', '', item)  # Remove "private" and any trailing spaces
+    item = re.sub(r'^[^a-zA-Z0-9\s]+', '', item)  # Remove any non-alphanumeric characters (except spaces) at the start of the string
+    item = re.sub(r'[-_]', ' ', item)  # Replace underscores and hyphens with spaces
+    return item.title()
