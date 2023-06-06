450833800f68c3fadfb7f97d2006b6c7e8bc143f
CV-Gate <miguel.mendoza@vizzuality.com>
2023-06-06 12:30:37 +0200

    updates TW

--- static/js/expandable.js
+++ static/js/expandable.js
@@ -1,48 +1,65 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // Attach click event to buttons with aria-controls attribute
-  $('[aria-controls]').on('click', function() {
+  $("[aria-controls]").on("click", function () {
     const button = $(this);
-    const targetId = button.attr('aria-controls').replace(' ', '-');
+    const targetId = button.attr("aria-controls").replace(" ", "-");
     const target = $(`#${targetId}`);
-    const targetUl = target.children('ul'); // Find the direct child ul element
-    const isExpanded = button.attr('aria-expanded') === 'true';
+    const targetUl = target.children("ul"); // Find the direct child ul element
+    const isExpanded = button.attr("aria-expanded") === "true";
 
     // Close other menus and reset their chevrons
-    $('[aria-controls]').not(this).each(function() {
-      const otherButton = $(this);
-      const otherTargetId = otherButton.attr('aria-controls');
-      const otherTarget = $(`#${otherTargetId}`);
-      const otherTargetUl = otherTarget.children('ul'); // Find the direct child ul element
+    $("[aria-controls]")
+      .not(this)
+      .each(function () {
+        const otherButton = $(this);
+        const otherTargetId = otherButton.attr("aria-controls");
+        const otherTarget = $(`#${otherTargetId}`);
+        const otherTargetUl = otherTarget.children("ul"); // Find the direct child ul element
 
-      if (!button.parents('li').has(otherButton).length) {
-        otherButton.attr('aria-expanded', false);
-        otherButton.find('svg').removeClass('rotate-90 text-gray-500').addClass('text-gray-400');
-        otherTargetUl.slideUp().addClass('collapsed-menu');
-      }
-    });
+        if (!button.parents("li").has(otherButton).length) {
+          otherButton.attr("aria-expanded", false);
+          otherButton
+            .find("svg")
+            .removeClass("rotate-90 text-gray-500")
+            .addClass("text-gray-400");
+          otherTargetUl.slideUp().addClass("collapsed-menu");
+        }
+      });
 
     // Collapse sibling elements
-    button.closest('li').siblings().each(function() {
-      const siblingButton = $(this).find('[aria-controls]').first();
-      const siblingTargetId = siblingButton.attr('aria-controls');
-      const siblingTarget = $(`#${siblingTargetId}`);
-      const siblingTargetUl = siblingTarget.children('ul'); // Find the direct child ul element
+    button
+      .closest("li")
+      .siblings()
+      .each(function () {
+        const siblingButton = $(this).find("[aria-controls]").first();
+        const siblingTargetId = siblingButton.attr("aria-controls");
+        const siblingTarget = $(`#${siblingTargetId}`);
+        const siblingTargetUl = siblingTarget.children("ul"); // Find the direct child ul element
 
-      siblingButton.attr('aria-expanded', false);
-      siblingButton.find('svg').removeClass('rotate-90 text-gray-500').addClass('text-gray-400');
-      siblingTargetUl.slideUp().addClass('collapsed-menu');
-    });
+        siblingButton.attr("aria-expanded", false);
+        siblingButton
+          .find("svg")
+          .removeClass("rotate-90 text-gray-500")
+          .addClass("text-gray-400");
+        siblingTargetUl.slideUp().addClass("collapsed-menu");
+      });
 
     // Toggle expanded state
-    button.attr('aria-expanded', !isExpanded);
+    button.attr("aria-expanded", !isExpanded);
 
     // Toggle chevron icon
     if (!isExpanded) {
-      button.find('svg').addClass('rotate-90 text-gray-500').removeClass('text-gray-400');
-      targetUl.removeClass('collapsed-menu').removeAttr('style');
+      button
+        .find("svg")
+        .addClass("rotate-90 text-gray-500")
+        .removeClass("text-gray-400");
+      targetUl.removeClass("collapsed-menu").removeAttr("style");
     } else {
-      button.find('svg').removeClass('rotate-90 text-gray-500').addClass('text-gray-400');
-      targetUl.addClass('collapsed-menu');
+      button
+        .find("svg")
+        .removeClass("rotate-90 text-gray-500")
+        .addClass("text-gray-400");
+      targetUl.addClass("collapsed-menu");
     }
   });
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+let colors = require("tailwindcss/colors");
+
 module.exports = {
-    content: [
-      "./templates/**/*.html",
-      "./static/**/*.js"
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-          sans: ['inter', 'sans-serif']
-        },
-        textColor: {
-          'vizzuality': '#2ba4a0',
-        },
-        backgroundColor: {
-          'vizzuality': '#2ba4a0',
-        },
+  content: ["./templates/**/*.html", "./static/**/*.js", "./*.py"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["inter", "sans-serif"],
+      },
+      colors: {
+        ...colors,
+        vizzuality: "#2ba4a0",
       },
     },
-    variants: {
-      extend: {},
+  },
+  variants: {
+    extend: {
+      display: ["group-hover"],
     },
-    plugins: [
-      require('@tailwindcss/forms'),
-      require('@tailwindcss/typography')
-    ]
-  }
-  
+  },
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+};
